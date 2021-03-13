04cfe531feb8c16c4d92071477b405b1414b73af
Lucie Trojanova <trojanova33@gmail.com>
2021-03-13 21:40:49 +0100

    ukol 1.pokus

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-// tady je místo pro náš program
+// úkol 1:
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+function zmenBarvu() {
+  document.querySelector(".ctverecek").style.backgroundColor = "green";
+}
+
+
+//úkol 2:
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
- *
+
  * @param {int} a
  * @param {int} b
  */
+
 function secti(a, b) {
   let c = a + b;
   return c;
 }
 
-/**
- * Upozorní uživatele při spuštění.
- */
-function upozorni() {
-  alert("Gratulace, právě jsi spustila tuto funkci!");
+document.querySelector("#vysledek").style.display = "none";
+
+function scitej() {
+document.querySelector("#vysledek").innerHTML = secti(4, 5);
+document.querySelector("#vysledek").style.display = "inline";
 }
+
+
+//úkol 3:
+function upozorni() {
+  let gratulace = "Gratulace, právě jsi spustila tuto funkci!";
+  console.log(gratulace);
+  document.querySelector(".ctverecek").textContent = "Gratulace, právě jsi spustila tuto funkci!";
+  document.querySelector(".ctverecek").style.lineHeight = "40px";
+}
+
